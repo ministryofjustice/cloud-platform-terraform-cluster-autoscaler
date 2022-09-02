d2f9b234c52fff8aac74a5af913a13996399ab07
Sablu Miah <sablu.miah@digital.justice.gov.uk>
2022-09-02 17:03:03 +0100

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -7,9 +7,5 @@ provider "aws" {
 module "example_cluster_autoscaler" {
   # source = ""github.com/ministryofjustice/cloud-platform-terraform-cluster-autoscaler?ref=0.2.0""
   source = "../"
-
-  cluster_domain_name         = data.terraform_remote_state.cluster.outputs.cluster_domain_name
-  eks_cluster_id              = data.terraform_remote_state.cluster.outputs.cluster_id
-  eks_cluster_oidc_issuer_url = data.terraform_remote_state.cluster.outputs.cluster_oidc_issuer_url
 }
 
