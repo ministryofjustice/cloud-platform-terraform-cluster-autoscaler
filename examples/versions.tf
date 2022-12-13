8d204c33ad99a99022d05ed4fc5fc9cc44f75f1a
jakemulley <jakemulley@users.noreply.github.com>
2022-12-13 17:16:57 +0000

    Fix examples

--- examples/versions.tf
+++ examples/versions.tf
@@ -6,8 +6,12 @@ terraform {
       version = ">=3.0.0"
     }
     helm = {
-      source = "hashicorp/helm"
-
+      source  = "hashicorp/helm"
+      version = ">=2.6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.12.1"
     }
   }
 }
