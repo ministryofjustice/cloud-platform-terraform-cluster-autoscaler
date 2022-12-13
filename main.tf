acda0934420a9227e216f49ea08130bf3aee4dff
jakemulley <jakemulley@users.noreply.github.com>
2022-12-13 17:15:55 +0000

    Refactor to meet standardised Terraform structure

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "iam_assumable_role_admin" {
   create_role                   = true
   role_name                     = "cas.${var.cluster_domain_name}"
   provider_url                  = var.eks_cluster_oidc_issuer_url
-  role_policy_arns              = [ length(aws_iam_policy.cluster_autoscaler) >= 1 ? aws_iam_policy.cluster_autoscaler.arn : ""]
+  role_policy_arns              = [length(aws_iam_policy.cluster_autoscaler) >= 1 ? aws_iam_policy.cluster_autoscaler.arn : ""]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:cluster-autoscaler"]
 }
 
